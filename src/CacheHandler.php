e5e3233975701fd1c85d869899b402e9a34df7f4
Dennis Krüger <mail@denniskr.de>
2025-05-06 02:07:36 +0200

    Add Apache 2.0 license header to all PHP files

--- src/CacheHandler.php
+++ src/CacheHandler.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * This file is part of easyTRIAS (https://github.com/dnnskrgr/easyTRIAS).
+ *
+ * Copyright 2025 Dennis Krüger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
 function getCachePath(string $stopPointRef, string $cacheDir): string {
     $fileName = 'departures_' . str_replace(':', '-', $stopPointRef) . '.txt';
